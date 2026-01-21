71920de940af608cc52d1bc2603e764a598192e3
sonugupta81 <sonuguptak2005@gmail.com>
2026-01-21 23:46:08 +0530

    new

--- src/Components/Nav/nav.jsx
+++ src/Components/Nav/nav.jsx
@@ -1,10 +1,10 @@
 
-import React, {useRef}from "react";
+import {useRef} from "react";
 import "./nav.css";
 import{ Link} from "react-scroll";
 
 function nav() {
-  let navRef = useRef();
+  let menuRef = useRef();
   let mobRef = useRef();
   
   
@@ -17,7 +17,8 @@ function nav() {
         <Link to="contact" activeClass="active" spy={true} smooth={true} duration={500}><li>Contact</li></Link>
         <Link to="about" activeClass="active" spy={true} smooth={true} duration={500}><li>About</li></Link>
       </ul>
-      <div className="hemburger" ref={navRef} onClick={()=>{mobRef.current.classList.toggle("activemob")
+      <div className="hemburger" ref={menuRef} onClick={()=>{
+        mobRef.current.classList.toggle("activemob")
         menuRef.current.classList.toggle("activehem")
       }}>
         <div className="hem"></div>
