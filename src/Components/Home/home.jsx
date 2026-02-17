b26f44b925ec28ef62b543b1af7b1916c4521ec3
sonugupta81 <sonuguptak2005@gmail.com>
2026-02-17 16:08:48 +0530

    add

--- src/Components/Home/home.jsx
+++ src/Components/Home/home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./home.css"
-import Man from "../../assets/image/sonuv.jpg";  
+import Man from "../../assets/image/sonuv.jpg";
 import TypingEffect from 'react-typing-effect';
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
@@ -8,27 +8,27 @@ import { useNavigate } from "react-router-dom";
 
 
 function home() {
-  useGSAP(()=>{
-    let tl1=gsap.timeline();
-    tl1.from(".line1",{
-      y:80,
-      direction:1,
-      opacity:0
+  useGSAP(() => {
+    let tl1 = gsap.timeline();
+    tl1.from(".line1", {
+      y: 80,
+      direction: 1,
+      opacity: 0
     })
-     tl1.from(".line2",{
-      y:80,
-      direction:1,
-      opacity:0
+    tl1.from(".line2", {
+      y: 80,
+      direction: 1,
+      opacity: 0
     })
-     tl1.from(".line3",{
-      y:80,
-      direction:1,
-      opacity:0
+    tl1.from(".line3", {
+      y: 80,
+      direction: 1,
+      opacity: 0
     })
-    gsap.from(".Righthome img",{
-      x:200,
-      direction:1,
-      opacity:0
+    gsap.from(".Righthome img", {
+      x: 200,
+      direction: 1,
+      opacity: 0
     })
   })
   return (
@@ -37,17 +37,13 @@ function home() {
         <div className="homedetails">
           <div className="line1">I'M</div>
           <div className="line2"> SONU K. GUPTA</div>
-          <div className="line3"> WEB DEVELOPER
-            {/* <TypingEffect
-            text={['A MERN Stack Developer', 'A Frontend Developer', 'A Backend Developer', 'Web Developer','Software Engineer']}
-            speed={500}
-            eraseSpeed={50}
-            eraseDelay={1000}
-            typingDelay={50}
-            cursor='_'
-            /> */}
+          <div className="line3">WEB DEVELOPER</div>
+          <div className="home-buttons">
+            
+            <a href="/sonu3.pdf" target="_blank" rel="noopener noreferrer" className="resume-btn">
+              <button className="hirebtn resume-btn">Hire Me</button>
+            </a>
           </div>
-          <button className="hirebtn">Hire Me</button>
         </div>
       </div>
       <div className="Righthome">
